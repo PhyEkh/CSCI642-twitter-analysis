4150e28787bab0a00892c0cdd57f9d50623d9e52
PhyEkh <ekheang.phy@go.stcloudstate.edu>
2022-04-17 16:52:21 -0500

    Done implemented import tweet dataset

---
+++ twitter sentiment analysis.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 16 14:56:45 2022
+
+@author: phyekheang
+"""
+
+import pandas as pd
+import numpy as np
+
+""" 
+we are using tweet dataset from kaggle: https://www.kaggle.com/competitions/tweet-sentiment-extraction/data?select=train.csv 
+
+
+""" 
+
+df1 = pd.read_csv("test.csv")
+df2 = pd.read_csv("train.csv")
